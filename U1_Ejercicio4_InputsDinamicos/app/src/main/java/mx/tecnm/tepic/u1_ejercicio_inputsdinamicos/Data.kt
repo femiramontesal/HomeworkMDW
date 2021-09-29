7e46b2097498ffcc60b5a8510f5aaa542affb060
Fer <femiramontesal@ittepic.edu.mx>
2021-09-28 23:26:02 -0600

    HW: 4 Dynamic inputs

--- U1_Ejercicio4_InputsDinamicos/app/src/main/java/mx/tecnm/tepic/u1_ejercicio_inputsdinamicos/Data.kt
+++ U1_Ejercicio4_InputsDinamicos/app/src/main/java/mx/tecnm/tepic/u1_ejercicio_inputsdinamicos/Data.kt
@@ -2,10 +2,21 @@ package mx.tecnm.tepic.u1_ejercicio_inputsdinamicos
 
 class Data(id: Int, nombre: String, domicilio: String) {
 
+
     var id=0
     var nombre =""
     var domicilio=""
 
+    fun contenido(): String {
+        return id.toString()+"&&C"+nombre+"&&C"+domicilio
+    }
+
+    fun construir(renglon: String) {
+        val contenido = renglon.split("&&C")
+        id = contenido[0].toString().toInt()
+        nombre = contenido[1]
+        domicilio = contenido[2]
+    }
 
 
 }
--- U1_Ejercicio4_InputsDinamicos/app/src/main/java/mx/tecnm/tepic/u1_ejercicio_inputsdinamicos/MainActivity.kt
+++ U1_Ejercicio4_InputsDinamicos/app/src/main/java/mx/tecnm/tepic/u1_ejercicio_inputsdinamicos/MainActivity.kt
@@ -4,21 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.text.InputType
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
     //declaramos los ArL
-    val vector = ArrayList<Data>()
-    val nombres = ArrayList<String>()
+    val vector = ArrayList<Data>() //Guardar los vectores a manera de objeto
+    val nombres = ArrayList<String>() //mostrar en el list view
 
-    var id = campoId.text.toString().toInt()
-    var nombre = campoNombre.text.toString()
-    var domicilio = campoDomicilio.text.toString()
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +24,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        btnInsertar.setOnClickListener{
+            insertar()
+        }
 
 
         btnEliminar.setOnClickListener {
@@ -33,30 +34,111 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        btnInsertar.setOnClickListener{
-            insertar()
-        }
 
         btnGuardar.setOnClickListener{
-           //
+           guardar()
+        }
+
+        btnAbrir.setOnClickListener{
+           var linea = abrir()
+            expandirContenido(linea)
         }
 
 
 
     }
 
-    fun insertar(){
+    private fun expandirContenido(contenido: String) {
+        var renglones = contenido.split("&&R")
+
+        vector.clear()
+        for (renglon in renglones){
+            var objetoData = Data(0,"","")
+            objetoData.construir(renglon)
+            vector.add(objetoData)
+            actualizarLista()
+        }
+    }
+
+    private fun abrir():String {
+        try {
+            val tarjetaSD = getExternalFilesDir(null)
+            val file = File(tarjetaSD!!.absolutePath,"archivo.txt")
+            val flujoArchivo = BufferedReader(InputStreamReader(FileInputStream(file)))
+
+            var linea = flujoArchivo.readLine()
+            flujoArchivo.close()
+            return linea
+
+        }catch (io:IOException){
+            AlertDialog.Builder(this).setTitle("Error")
+                .setMessage("Mensaje ${io.message}")
+                .setPositiveButton("Aceptar"){d,i->d.dismiss()}
+                .show()
+            return ""
+        }
+    }
+
+    private fun guardar() {
+        var dataVector =""
+        var total = vector.size-1
+
+        (0..total).forEach {
+            val objetoData = vector.get(it)
+            dataVector+= objetoData.contenido()+"&&R"
+        }
+        dataVector = dataVector.substring(0,dataVector.lastIndexOf("&&R"))
+
+        try {
+            val tarjetaSD = getExternalFilesDir(null)
+            val file = File(tarjetaSD!!.absolutePath,"archivo.txt")
+            val flujoArchivo = OutputStreamWriter(FileOutputStream(file))
+
+            flujoArchivo.write(dataVector)
+            flujoArchivo.flush()
+            flujoArchivo.close()
+
+            AlertDialog.Builder(this).setTitle("Exito")
+                .setMessage("Si se gaurdo")
+                .setPositiveButton("Aceptar"){d,i->d.dismiss()}
+                .show()
+        }catch (io:IOException){
+            Toast.makeText(this,"Error ${io.message}",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun insertar(){
+
+        var objetoDatos = Data(0,"","")
+
+        objetoDatos.id   = campoId.text.toString().toInt()
+        objetoDatos.nombre = campoNombre.text.toString()
+        objetoDatos.domicilio = campoDomicilio.text.toString()
 
 
+        vector.add(objetoDatos)
+
+        actualizarLista()
+        campoDomicilio.setText("")
+        campoId.setText("")
+        campoNombre.setText("")
+
+    }
+
+    private fun actualizarLista() {
+        nombres.clear()
+        var total = vector.size-1
+
+        (0..total).forEach {
+            nombres.add(vector[it].nombre)
+        }
+
+        lista.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,nombres)
 
 
-        //cualquiera de las 2 jala
-        //val datos: Data = Data(id,nombre,domicilio)
-        //vector.add(datos)
-
-        vector.add(Data(id,nombre,domicilio))
-
-
+        lista.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(this,"Diste click en ${vector[i].id}",Toast.LENGTH_LONG).show()
+        }
     }
 
     fun eliminar(){
@@ -70,7 +152,8 @@ class MainActivity : AppCompatActivity() {
             .setMessage("¿Cuál campo quiere eliminar?")
             .setView(cantidad)
             .setPositiveButton("Eliminar"){d,i->
-                // aquí va la función que elimina
+                vector.removeAt(cantidad.text.toString().toInt()-1)
+                actualizarLista()
                 d.dismiss()
             }
             .setNegativeButton("Cancelar"){d,i->
